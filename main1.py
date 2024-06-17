701b797525cc99199ba26d8c2a41447efedd6c5f
PSVFSD <psvfsd@gmail.com>
2024-06-17 11:14:42 +0300

    first_commit

--- main1.py
+++ main1.py
@@ -1,7 +1,2 @@
+print("Attempt number 1")
 print("Attempt number 2")
-print("and again")
-print("and once again")
-print("and the last for today")
-print("one more last for today")
-a = "hello"
-print(a)
